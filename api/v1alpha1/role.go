ba4e01d8cb85b9500bf2840c4d3e24caba968999
nilsorbat <nils@orbat.sh>
2021-05-17 17:09:18 +0200

    Rework API for grants

--- api/v1alpha1/role.go
+++ api/v1alpha1/role.go
@@ -1,7 +1,7 @@
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 //+kubebuilder:object:root=true
@@ -9,53 +9,61 @@ import (
 
 // Role is the Schema for the roles API
 type Role struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+    metav1.TypeMeta   `json:",inline"`
+    metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   RoleSpec   `json:"spec"`
-	Status RoleStatus `json:"status,omitempty"`
+    Spec   RoleSpec   `json:"spec"`
+    Status RoleStatus `json:"status,omitempty"`
 }
 
 type RoleSpec struct {
-	InstanceRef InstanceRef `json:"instanceRef"`
-	RoleName    string      `json:"roleName"`
-	//+kubebuilder:validation:Optional
-	//+kubebuilder:default:=true
-	PreventDeletion bool `json:"preventDeletion"`
-	//+kubebuilder:validation:Optional
-	//+kubebuilder:default:=false
-	CascadeDelete bool `json:"cascadeDelete"`
-	//+kubebuilder:validation:Optional
-	Options []string `json:"options"`
-	//+kubebuilder:validation:Optional
-	Password string `json:"password"`
-	//+kubebuilder:validation:Optional
-	PasswordRef PasswordRef `json:"passwordRef"`
-	//+kubebuilder:validation:Optional
-	Grants []Grant `json:"grants"`
+    InstanceRef InstanceRef `json:"instanceRef"`
+    RoleName    string      `json:"roleName"`
+    //+kubebuilder:validation:Optional
+    //+kubebuilder:default:=true
+    PreventDeletion bool `json:"preventDeletion"`
+    //+kubebuilder:validation:Optional
+    //+kubebuilder:default:=false
+    CascadeDelete bool `json:"cascadeDelete"`
+    //+kubebuilder:validation:Optional
+    Options []string `json:"options"`
+    //+kubebuilder:validation:Optional
+    Password string `json:"password"`
+    //+kubebuilder:validation:Optional
+    PasswordRef PasswordRef `json:"passwordRef"`
+    //+kubebuilder:validation:Optional
+    Grants []Grant `json:"grants"`
 }
 
 type PasswordRef struct {
-	Name        string `json:"name"`
-	PasswordKey string `json:"passwordKey"`
+    Name        string `json:"name"`
+    PasswordKey string `json:"passwordKey"`
 }
 
 type Grant struct {
-	Database   string   `json:"database,omitempty"`
-	Schema     string   `json:"schema,omitempty"`
-	ObjectType string   `json:"objectType"`
-	Privileges []string `json:"privileges"`
+    Database string        `json:"database"`
+    Objects  []GrantObject `json:"objects"`
+}
+
+type GrantObject struct {
+    Identifier string   `json:"identifier"`
+    Type       string   `json:"type"`
+    Privileges []string `json:"privileges"`
+    //+kubebuilder:validation:Optional
+    WithGrantOption bool `json:"withGrantOption"`
+    //+kubebuilder:validation:Optional
+    WithAdminOption bool `json:"withAdminOption"`
 }
 
 type RoleStatus struct {
-	Status string `json:"status"`
+    Status string `json:"status"`
 }
 
 //+kubebuilder:object:root=true
 
 // RoleList contains a list of Role
 type RoleList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Role `json:"items"`
+    metav1.TypeMeta `json:",inline"`
+    metav1.ListMeta `json:"metadata,omitempty"`
+    Items           []Role `json:"items"`
 }
