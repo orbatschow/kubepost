3b0cb28366d299640ded71431be656e2f2d6052d
Mathis Rudolf <mathis.rudolf@credativ.de>
2021-08-10 15:18:11 +0200

    Add function SubtractPrivilegeIntersection

--- repository/util.go
+++ repository/util.go
@@ -19,3 +19,60 @@ func StringArrayToPrivilegArray(sa []string) []v1alpha1.Privilege {
     return buffer
 }
 
+// This function will compare two grantObjects and will remove priviliges if
+// both would grant them. Returning the number of found intersections.
+func SubtractPrivilegeIntersection(a, b *v1alpha1.GrantObject) int {
+    var aBuffer []v1alpha1.Privilege
+    var bBuffer []v1alpha1.Privilege
+    counter := 0
+
+    if a.Identifier != b.Identifier {
+        return 0
+    }
+
+    if a.Type != b.Type {
+        return 0
+    }
+
+    if a.Schema != b.Schema {
+        return 0
+    }
+
+    privilegeIntersectionMap := map[v1alpha1.Privilege]int{}
+
+    for _, privilege1 := range a.Privileges {
+
+        for _, privilege2 := range b.Privileges {
+
+            if privilege1 == privilege2 {
+                _, contains := privilegeIntersectionMap[privilege1]
+                if !contains {
+                    privilegeIntersectionMap[privilege1] = 0
+                }
+                privilegeIntersectionMap[privilege1]++
+                counter++
+            }
+        }
+    }
+
+    for _, privilege := range a.Privileges {
+        _, contains := privilegeIntersectionMap[privilege]
+        if !contains {
+            aBuffer = append(aBuffer, privilege)
+        }
+    }
+
+    a.Privileges = aBuffer
+
+    for _, privilege := range b.Privileges {
+        _, contains := privilegeIntersectionMap[privilege]
+        if !contains {
+            bBuffer = append(bBuffer, privilege)
+        }
+    }
+
+    b.Privileges = bBuffer
+
+    return counter
+}
+
