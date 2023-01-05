d52700d2361c0c3b732cc0e0bdad93a392212b25
ntap-fge <117348352+ntap-fge@users.noreply.github.com>
2023-01-05 10:25:50 +0100

    Escape values in Postgres connect URL (#45)
    - rework connection initialization
    - remove legacy postgres struct
    - escape values in postgres connection

--- pkg/connection/connection.go
+++ pkg/connection/connection.go
@@ -3,17 +3,17 @@ package connection
 import (
 	"context"
 	"fmt"
+	"net/url"
+
 	"github.com/jackc/pgx/v4"
 	"github.com/orbatschow/kubepost/api/v1alpha1"
 	"github.com/orbatschow/kubepost/pkg/namespace"
-	"github.com/orbatschow/kubepost/pkg/postgres"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"strconv"
 )
 
 func List(ctx context.Context, ctrlClient client.Client, connectionNamespaceSelector metav1.LabelSelector, connectionSelector metav1.LabelSelector) ([]v1alpha1.Connection, error) {
@@ -68,33 +68,25 @@ func GetConnection(ctx context.Context, client client.Client, connection *v1alph
 	if usernameBytes == nil {
 		return nil, fmt.Errorf("could not parse username for connection '%s/%s' from secret '%s/%s", connection.ObjectMeta.Namespace, connection.ObjectMeta.Name, connection.ObjectMeta.Namespace, usernameSecret.ObjectMeta.Name)
 	}
+	username := string(usernameBytes)
 
 	passwordBytes := passwordSecret.Data[connection.Spec.Password.Key]
 	if passwordBytes == nil {
 		return nil, fmt.Errorf("could not parse password for connection '%s/%s' from secret '%s/%s", connection.ObjectMeta.Namespace, connection.ObjectMeta.Name, connection.ObjectMeta.Namespace, usernameSecret.ObjectMeta.Name)
 	}
-
-	p := postgres.Postgres{
-		Host:     connection.Spec.Host,
-		Port:     strconv.Itoa(connection.Spec.Port),
-		Username: string(usernameBytes),
-		Password: string(passwordBytes),
-		Database: connection.Spec.Database,
-		SSLMode:  connection.Spec.SSLMode,
-	}
+	password := string(passwordBytes)
 
 	conn, err := pgx.Connect(context.Background(), fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=%s&application_name=kubepost",
-		p.Username,
-		p.Password,
-		p.Host,
-		p.Port,
-		p.Database,
-		p.SSLMode,
+		"postgres://%s@%s:%d/%s?sslmode=%s&application_name=kubepost",
+		url.UserPassword(username, password).String(),
+		connection.Spec.Host,
+		connection.Spec.Port,
+		url.PathEscape(connection.Spec.Database),
+		connection.Spec.SSLMode,
 	),
 	)
 	if err != nil {
-		return nil, fmt.Errorf("unable to connect to database: '%s' on host '%s' with user '%s' : '%s'", p.Database, p.Host, p.Username, err)
+		return nil, fmt.Errorf("unable to connect to database: '%s' on host '%s' with user '%s' : '%s'", connection.Spec.Database, connection.Spec.Host, username, err)
 	}
 
 	return conn, nil
